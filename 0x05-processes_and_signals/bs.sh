e646742db15ced1b9698996720c9fd603e033652
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-27 10:38:24 +0100

    terminating files using the pkill command and trapping signals

---
+++ 0x05-processes_and_signals/bs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# positional.sh
+# This script reads 3 positional parameters and prints them out.
+
+echo "$1 is the first positional parameter, \$1."
+echo "$2 is the second positional parameter, \$2."
+echo "$3 is the third positional parameter, \$3."
+echo
+echo "The total number of positional parameters is $#."
