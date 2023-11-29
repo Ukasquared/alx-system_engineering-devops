4446588ebaf96f9ca7456e00073f7e466ab9f1b3
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-29 05:27:48 +0100

    search for hbt.....n string"

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# regex that matches a string
+puts ARGV[0].scan(/hb?tn/).join
---
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# regex that matches a string
+puts ARGV[0].scan(/hbt+n/).join
