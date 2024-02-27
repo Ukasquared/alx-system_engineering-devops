0ec285924991fc586733944f5f18ca4832c6310c
Ukasquared <ukachukwujudith95@gmail.com>
2024-02-27 06:49:43 +0100

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # puppet manifest for creating a file in /tmp
 file {'/tmp/school':
-    ensure => 'file',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet',
-} 
+ensure  => 'file',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+}
