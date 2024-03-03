77dcde6534b1077e3387e803d174c15876429794
Ukasquared <ukachukwujudith95@gmail.com>
2024-03-03 06:28:02 +0100

    flask

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,8 @@
 # kill a process
 
 exec { 'kill_me_now':
-command     => 'pkill -f killmenow',
-refreshonly =>  true,
-logoutput   =>  true,
+command   => 'pkill -f killmenow',
+path      => '/usr/bin',
+logoutput =>  true,
+provider  => 'shell',
 }
