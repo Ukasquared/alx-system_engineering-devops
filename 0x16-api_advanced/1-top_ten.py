57b3bd41792636dbc2a9652abdd09bfacffdba2c
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-05-09 16:35:46 +0100

    print the top ten hot post

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,20 +5,24 @@ a specific subreddit
 """
 import requests
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """ retrieve number of
     for a particular subreddit
     """
-    header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64)
-               AppleWebKit/537.36 (KHTML, like Gecko)
-               Chrome/124.0.0.0 Safari/537.36'}
-    end_point = "r/{}/about.json".format(subreddit)
+    header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64)\
+               AppleWebKit/537.36 (KHTML, like Gecko)\
+               Chrome/124.0.0.0 Safari/537.36"}
+    end_point = "r/{}/hot.json".format(subreddit)
     url = "https://www.reddit.com/{}".format(end_point)
     try:
         response = requests.get(url, headers=header)
+        if response.status_code == 301:
+            print ("None")
         to_json = response.json()
-        subscribers = to_json.get(subscribers)
-        if subscribers is not None:
-            return subscribers
+        # get data key
+        data_list = to_json.get("data").get("children")
+        for i in range(0, 9):
+            post_title = data_list[i].get("data").get("title")
+            print (post_title)
     except Exception:
-        return 0
+        print ("None")
